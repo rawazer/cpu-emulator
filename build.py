30ec416b8be97d9f30357b0364a858f255c3a644
rawazer <roireit@gmail.com>
2026-02-26 22:39:13 +0200

    build script

---
+++ build.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import subprocess
+
+DEFAULT_PROGRAM_CREATOR = "example_programs/default/example_program_creator.c"
+
+def main():
+    os.makedirs("out", exist_ok=True)
+    prg_crt_cmpl_res = subprocess.run(["gcc", DEFAULT_PROGRAM_CREATOR, "-o", "out/prg_creator.bin"])
+    if (prg_crt_cmpl_res.returncode != 0):
+        sys.exit(f"prg_crt_cmpl_res: {prg_crt_cmpl_res.returncode}")
+    prg_crt_res = subprocess.run(["./out/prg_creator.bin"])
+    if (prg_crt_res.returncode != 0):
+        sys.exit(f"prg_crt_res: {prg_crt_res.returncode}")
+    cpu_cmpl_res = subprocess.run(["gcc", "main.c", "cpu/cpu.c", "program_loader/program_loader.c", "-o", "out/cpu.bin"])
+    if (cpu_cmpl_res.returncode != 0):
+        sys.exit(f"cpu_cmpl_res: {cpu_cmpl_res.returncode}")
+
+if __name__ == "__main__":
+    main()
